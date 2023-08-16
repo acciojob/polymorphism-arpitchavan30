03f6beece24f8b509b9c1675a88ab27935542a33
arpitchavan <arpitchvn@gmail.com>
2023-08-16 15:31:12 +0530

    attempt2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,18 @@ package com.driver;
 
 public class Main {
 
+    public static class product{
+        public int product(int x, int y) { return x*y;}
+        public int product(int x, int y, int z) { return x*y*z;}
+        public double product(double x, double y) {return x*y;}
+
+    }
+
+    public static void main(String[] args){
+        product prod = new product();
+        prod.product(5,10);
+        prod.product(5,10,5);
+        prod.product(5.2,5.6);
+    }
+
 }
